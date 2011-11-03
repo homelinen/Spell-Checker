ac5337d5565fadfd76ff9abe822b3a73516dfe5e
Calum Gilchrist <calum@homelinen.org>
2011-11-03 13:32:55 +0000

    Initiated the repository, with some bare bones files for the project.

---
+++ src/spellcheck/Dictionary.java
@@ -0,0 +1,24 @@
+//Interface your dictionary has to implement
+
+package spellcheck;
+
+import java.util.Iterator;
+
+public interface Dictionary {
+
+    // throws exception if key is alreadyPresent
+    public void insert(String key) throws DictionaryException;
+    
+    // Removes the entry with the specified key from the dictionary. Throws 
+    // DictionaryException if no entry with key in the dictionary          
+    public void remove(String key)throws DictionaryException;
+
+    /// Returns true if there is entry with specified key in the dictionary 
+    public boolean find(String key);
+    
+   
+    // Returns iterator object over all elements stored in the dictionary
+    public Iterator elements();
+
+}
+
