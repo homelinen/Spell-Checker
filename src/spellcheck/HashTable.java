1a86c6c073b79232f9beb476433ef37804a41894
Calum Gilchrist <calum@homelinen.org>
2011-11-18 15:02:44 +0000

    Changed add to insert, added a double hashing method

--- src/spellcheck/HashTable.java
+++ src/spellcheck/HashTable.java
@@ -10,6 +10,9 @@ public class HashTable<K,V> {
 
 	private ArrayList<V> table;
 	
+	//Should this be a field?
+	private StringHashCode hashCode;
+	
 	public HashTable() {
 				
 	}
@@ -22,8 +25,63 @@ public class HashTable<K,V> {
 		
 	}
 
-	public void add(V value) {
+	/**
+	 * Add a value to the HashMap
+	 * @param value
+	 */
+	public void insert(V value) {
+		int hash = hashCode.giveCode(value);
 		
+		int count = 0;
+		
+		boolean valid=false;
+		hash= compress(hash);
+		
+		//Create a copy of the Compressed hash
+		int compressed = hash;
+		
+		while (!valid) {
+			if (containsKey(hash)) {
+				count++;
+				hash = compressAgain(compressed, hash, count);
+			} else {
+				valid=true;
+			}
+		}
+		
+		/*
+		 * Need to check load factor here, or somewhere
+		 */
+		
+		table.add(hash, value);
+	}
+	
+	/**
+	 * Compresses a hash code once
+	 */
+	public int compress(int hash) {
+		int a = 241;
+		int b = 13;
+		
+		int compress = (a * hash) + b;
+		return compress;
+	}
+	
+	/**
+	 * Hash the code a second, third, nth time (specified by j)
+	 * @param k
+	 * @param hash
+	 * @param j
+	 * @return
+	 */
+	public int compressAgain(int k, int hash, int j) {
+		
+		//Half the size and find the next prime
+		int d = findNextPrime(size()/2);
+		
+		int compress =  ((d - (k % d)) + (j * hash)) % size(); 
+		
+		return compress;
 	}
 	
 	public V remove() {
